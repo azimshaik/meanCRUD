faf148171b8c464d28277888043576f0111df0ea
azimshaik <cheer.buddu@gmail.com>
2017-01-25 17:49:17 -0500

    mongo connection to mlad :(

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ console.log("May node be with you");
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+var db
+// MongoClient.connect('mongodb://azim:Az1001*#im@ds131119.mlab.com:31119/star-wars-quotes',function(err,database){
+// 	if(err) 
+// 		throw err;
+// 	db = database
+// 	app.listen(3000,function(){
+// 		console.log('listening on 3000')
+// 	})
 
+// })
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.listen(3000,function(){
 	console.log('listening on 3000');
 })
@@ -16,5 +27,9 @@ app.get('/',function(req,res){
 	
 })
 app.post('/quotes',function(req,res) {
-	console.log(req.body);
+	db.collection('quotes').save(req.body, function(err, result){
+		if(err) return console.log(err);
+		console.log('saved to database')
+		res.redirect('/')
+	})
 })
