3086fcce2904ca7d681f75bf6b396d48bd268dbc
azimshaik <cheer.buddu@gmail.com>
2017-02-02 06:32:41 -0500

    mongoLab get updta,delete

--- routes/tasks.js
+++ routes/tasks.js
@@ -24,6 +24,63 @@ router.get('/tasks/:id',function(req,res,next){
         	res.json(task);
         });
 });
+//Save task
+
+router.post('/tasks',function(req,res,next){
+	var task = req.body;
+	if(!task.title ||(task.isDone + '')){
+		res.status(400);
+		res.json({
+			"error": "Bad Data"
+		});
+	}else{
+		db.tasks.save(task, function(err,task){
+			if(err){
+        		res.send(err);
+        	}
+        	res.json(task);
+		});
+	}
+});
+
+//Delete Task
+router.delete('/tasks/:id',function(req,res,next){
+        //res.send('TASKS API');
+        db.tasks.remove({_id: mongojs.ObjectId(req.params.id)},function(err,task){
+        	if(err){
+        		res.send(err);
+        	}
+        	res.json(task);
+        });
+});
+
+//Update Task
+router.put('/tasks/:id',function(req,res,next){
+		var task = req.body;
+		var updTask = {};
+		if(task.isDone){
+			updTask.isDone = task.isDone;
+		}
+		if(task.title){
+			updTask.title = task.title;
+		}
+		if(!updTask){
+			res.status(400);
+			res.json({
+				"error":"Bad Data"
+			});
+		}else{
+			db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask,{}, function(err,task){
+        	if(err){
+        		res.send(err);
+        	}
+        	res.json(task);
+        });
+
+		}
+        //res.send('TASKS API');
+        
+});
 module.exports = router;
 
 
