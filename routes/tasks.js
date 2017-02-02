9651148880f0b780b4feb9e370819f38187a1c9c
azimshaik <cheer.buddu@gmail.com>
2017-02-01 21:00:02 -0500

    mongoLab get single task works

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongojs = require('mongojs');
 
 var db = mongojs('mongodb://azimshaik:password@ds131119.mlab.com:31119/star-wars-quotes',['tasks']);
-
+//Get All Tasks
 router.get('/tasks',function(req,res,next){
         //res.send('TASKS API');
         db.tasks.find(function(err,tasks){
@@ -14,6 +14,16 @@ router.get('/tasks',function(req,res,next){
         });
 });
 
+//Get Single Task
+router.get('/tasks/:id',function(req,res,next){
+        //res.send('TASKS API');
+        db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err,task){
+        	if(err){
+        		res.send(err);
+        	}
+        	res.json(task);
+        });
+});
 module.exports = router;
 
 
